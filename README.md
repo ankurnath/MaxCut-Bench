6ce1c1aca61cfde866036283ab59af7dfb4838da
Ankur Nath <anath@csce-44q58y3.engr.tamu.edu>
2024-06-06 15:28:51 -0500

    Readme fixed

--- README.md
+++ README.md
@@ -50,39 +50,3 @@ interface. Currently, we support the following solvers:
 ### Greedy
 
 
-## Repository Contents
-
-In `solvers`, you can find the wrappers for the currently supported solvers. In `data_generation`, you find the code required for generating random and real-world graphs.
-
-For using this suite, `conda` is required. You can the `setup_bm_env.sh` script which will setup the conda environment with all required dependencies. You can find out more about the usage using `python main.py -h`. The `main.py` file is the main interface you will call for data generation, solving, and training.
-
-In the `helper_scripts` folder, you find some scripts that could be helpful when doing analyses with this suite.
-
-## Publication
-
-You can find our ICLR 2022 conference paper [here](https://openreview.net/forum?id=mk0HzdqY7i1).
-
-If you use this in your work, please cite us and the papers of the solvers that you use.
-
-```bibtex
-@inproceedings{boether_dltreesearch_2022,
-  author = {Böther, Maximilian and Kißig, Otto and Taraz, Martin and Cohen, Sarel and Seidel, Karen and Friedrich, Tobias},
-  title = {What{\textquoteright}s Wrong with Deep Learning in Tree Search for Combinatorial Optimization},
-  booktitle = {Proceedings of the International Conference on Learning Representations ({ICLR})},
-  year = {2022}
-}
-```
-
-If you have questions you are welcome to reach out to [@MaxiBoether](https://github.com/MaxiBoether) and [@EightSQ](https://github.com/EightSQ).
-
-### Data and Models
-
-On popular request, we provide the (small) random graphs with labels and the models we trained [here](https://owncloud.hpi.de/s/cv6szEJtSs8UGju) ([backup location](https://mboether.com/paper-models-randomgraphs.zip)).
-The Intel tree search model that was trained by Li et al. can be downloaded from [the original repository](https://github.com/isl-org/NPHard/tree/master/model).
-Note that we cannot reupload the labeled real world graphs, as we do not have any permission to re-distribute them.
-However, the benchmarking suite supports the automatic download and labeling of _all_ random and real world graphs used in the paper.
-Please do not only rely on the data we provide and instead use this suite to generate graphs and train models on your own, as there is no guarantee that our evaluation is fully correct.
-
-## Contributions
-
-There are (of course) some improvements that can be made. For example, the argument parsing requires a major refactoring, and the output formats are currently not fully harmonized. We are open for pull requests, if you want to contribute. Thank you very much!
---
+++ solvers/CIM/CIM.py
@@ -0,0 +1,130 @@
+
+
+
+from cim_optimizer.solve_Ising import *
+import pickle
+import numpy as np
+import pandas as pd
+import torch
+import glob
+from scipy.sparse import load_npz
+import random
+
+
+
+
+class GraphDataset(object):
+
+    def __init__(self,folder_path,ordered=False):
+        super().__init__()
+
+        self.file_paths=glob.glob(f'{folder_path}/*.npz')
+        self.file_paths.sort()
+        self.ordered=ordered
+
+        if self.ordered:
+            self.i = 0
+
+    def __len__(self):
+        return len(self.file_paths)
+    
+    def get(self):
+        if self.ordered:
+            file_path = self.file_paths[self.i]
+            self.i = (self.i + 1)%len(self.file_paths)
+        else:
+            file_path = random.sample(self.file_paths, k=1)[0]
+        return load_npz(file_path).toarray()
+
+
+
+
+def compute_cut(matrix,spins):
+  return (1/4) * np.sum( np.multiply(matrix, 1 - np.outer(spins,spins)))
+
+def load_pickle(file_path):
+  with open(file_path, 'rb') as f:
+      data = pickle.load(f)
+  return data
+
+from cim_optimizer.optimal_params import maxcut_100_params
+from cim_optimizer.optimal_params import maxcut_200_params
+from cim_optimizer.optimal_params import maxcut_500_params
+
+
+def solve(graph,hyperparameters):
+    
+
+    result=Ising(-graph).solve(hyperparameters_autotune=True,
+                           hyperparameters_randomtune=False,
+                           return_lowest_energies_found_spin_configuration=True,
+                           return_lowest_energy_found_from_each_run=False,
+                           return_spin_trajectories_all_runs=False,
+                           return_number_of_solutions=1,
+                           suppress_statements=True,
+                           **hyperparameters)
+
+    
+    spins=result.result['lowest_energy_spin_config']
+    cut= (1/4) * np.sum( np.multiply(graph, 1 - np.outer(spins, spins) ) )
+    return cut,spins
+
+from argparse import ArgumentParser
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('--distribution', type=str,default="WattsStrogatz_200vertices_weighted",  help='Distribution of dataset')
+    parser.add_argument("--device", type=int,default=None, help="cuda device")
+    parser.add_argument("--num_runs",type=int,default=100,help="Number of runs per instance")
+    parser.add_argument("--time_span",type=int,default=1200,help="Time span")
+    parser.add_argument("--num_parallel_runs",type=int,default=20,help="Number of parallel runs")
+    parser.add_argument("--model",type=str,default="CAC", help="model")
+
+    args = parser.parse_args()
+
+    hyperparameters=maxcut_200_params()
+    hyperparameters['num_runs']=args.num_runs
+    hyperparameters['num_timesteps_per_run']=  args.time_span
+    hyperparameters['num_parallel_runs']=  args.num_parallel_runs
+
+
+    if torch.cuda.is_available():
+        hyperparameters['use_GPU']=True
+        if args.device is None:
+            device = 'cuda:0' 
+        else:
+            device=f'cuda:{args.device}'
+    else:
+        device='cpu'
+
+    if args.model=='CAC':
+        hyperparameters['use_CAC']=True
+    elif args.model=='AHC':
+        hyperparameters['use_CAC']=False
+    else:
+        raise ValueError("Unknown options")
+    hyperparameters['chosen_device']=device
+
+        
+    dataset=GraphDataset(folder_path=f'../data/testing/{args.distribution}',ordered=True)
+    print ('Number of test graphs:',len(dataset))
+       
+    cuts=[]
+    spins=[]
+
+
+    for _ in range(len(dataset)):
+        graph=dataset.get()
+        cut,spin=solve(graph,hyperparameters)
+        cuts.append(cut)
+        spins.append(spin)
+
+
+    df={'cut':cuts,'Solution':spins}
+    df=pd.DataFrame(df)
+    print(df)
+    save_folder=f"pretrained agents/{args.distribution}_{args.model}/data"
+    os.makedirs(save_folder,exist_ok=True)
+    
+    df.to_pickle(os.path.join(save_folder,'results'))
+
